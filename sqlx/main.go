8dac96a452e65e81239e1cc93cf19120937f02e1
JeffWu <itmrchow@gmail.com>
2024-07-10 18:02:32 +0800

    feat: create "checkDbExists"
    檢查db有沒有存在 , 沒有存在之後要做create

--- sqlx/main.go
+++ sqlx/main.go
@@ -8,13 +8,44 @@ import (
 )
 
 func main() {
+
 	log.Println("connect db")
-	_, err := connectDb()
+	db, err := connectDb()
 	if err != nil {
 		log.Fatalln("DB error: " + err.Error())
 	}
 	log.Println("server up")
 
+	// create sqlx_test_db database
+	dbExist, checkExistsErr := checkDbExists(db)
+	if checkExistsErr != nil {
+		log.Fatalln("DB error: " + checkExistsErr.Error())
+	}
+
+	if dbExist {
+		log.Println("db 在哦")
+	} else {
+		// create schema
+		log.Println("db 建起來")
+
+		log.Println("db 建好了")
+	}
+
+	// if err := createSchema(db); err != nil {
+	// 	log.Fatalln("DB error: " + err.Error())
+	// }
+
+	// start server
+
+}
+
+func checkDbExists(db *sqlx.DB) (bool, error) {
+	var exists bool
+	err := db.Get(&exists, "SELECT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'sqlx_test_db')")
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
 }
 
 func connectDb() (*sqlx.DB, error) {
