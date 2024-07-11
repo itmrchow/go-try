bec5cccf7c8f2fb6dffade81a86fdd1e33f8598b
JeffWu <itmrchow@gmail.com>
2024-07-11 17:19:42 +0800

    feat: insert sample
    insertPost 新增單筆透過struct
    insertPosts 多單筆透過struct

--- sqlx/crud/main.go
+++ sqlx/crud/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,10 +15,50 @@ func main() {
 	}
 	log.Println("connected db")
 
-	createUser(db)
+	// insertUser(db)
 
-	// createPost(db)
+	// insertPost(db)
+	insertPosts(db)
 
+	// updatePost(db)
+
+}
+
+type Post struct {
+	PostId    int    `db:"post_id"`
+	UserId    int    `db:"user_id"`
+	Title     string `db:"title"`
+	Content   string `db:"content"`
+	CreatedAt int64  `db:"created_at"`
+	UpdatedAt int64  `db:"updated_at"`
+	DeletedAt int64  `db:"deleted_at"`
+}
+
+func insertPost(db *sqlx.DB) {
+	log.Println("Create Post")
+
+	insertStr := "INSERT INTO `sqlx_test_db`.`posts` (`user_id`, `title`, `content` ) VALUES (:user_id, :title, :content)"
+	_, err := db.NamedExec(insertStr, &Post{UserId: 1, Title: "PostTitle0", Content: "Content0"})
+
+	if err != nil {
+		log.Fatalln("Insert error:", err.Error())
+	}
+}
+
+func insertPosts(db *sqlx.DB) {
+	log.Println("Create Post")
+
+	posts := []Post{}
+
+	for i := 0; i < 100; i++ {
+		posts = append(posts, Post{UserId: 1, Title: fmt.Sprintf("PostTitle%d", i), Content: fmt.Sprintf("Content%d", i)})
+	}
+
+	insertStr := "INSERT INTO `sqlx_test_db`.`posts` (`user_id`, `title`, `content` ) VALUES (:user_id, :title, :content)"
+	_, err := db.NamedExec(insertStr, posts)
+	if err != nil {
+		log.Fatalln("Insert error:", err.Error())
+	}
 }
 
 type User struct {
@@ -29,7 +70,8 @@ type User struct {
 	UpdatedAt int64  `db:"updated_at"`
 }
 
-func createUser(db *sqlx.DB) {
+func insertUser(db *sqlx.DB) {
+	log.Println("Create user")
 
 	insertStr := "INSERT INTO `sqlx_test_db`.`users` (`username`, `email`, `password` ) VALUES (:username, :email, :password)"
 	_, err := db.NamedExec(insertStr, &User{Username: "Jojo", Email: "Jojo@gmail.com", Password: "pwd"})
